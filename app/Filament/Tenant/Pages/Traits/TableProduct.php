9cabfc8cc033fb6520cbc67b569a0574feaea14e
mdaushi <firdaus.ex19@gmail.com>
2026-01-01 13:47:38 +0800

    fix: remove 'barcode' from searchable fields in product table

--- app/Filament/Tenant/Pages/Traits/TableProduct.php
+++ app/Filament/Tenant/Pages/Traits/TableProduct.php
@@ -58,7 +58,7 @@ trait TableProduct
                         ->columnStart(0),
                     TextColumn::make('name')
                         ->size('lg')
-                        ->searchable(['sku', 'name', 'barcode'])
+                        ->searchable(['sku', 'name'])
                         ->extraAttributes([
                             'class' => 'font-bold',
                         ]),
